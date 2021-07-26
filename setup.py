ecf0efff8d5a4064ced3a549754d5e09e0cb4a39
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-07-26 15:17:36 +0200

    update rust-sgx-gen version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         'aiofile==3.3.3',
         'pycryptodome==3.10.1',
         'reactive-net==0.2',
-        'rust-sgx-gen==0.1.3',
+        'rust-sgx-gen==0.1.4',
         'PyYAML==5.4.1'
     ],
     classifiers=[
