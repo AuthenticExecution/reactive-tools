06e87db144f1f5324ddcc5b5d0449a5d7f31024d
gianlu33 <36645341+gianlu33@users.noreply.github.com>
2021-08-06 14:31:47 +0200

    Manager config on a separate file (#26)
    * manager config in a separate file
    
    * fix

--- reactivetools/config.py
+++ reactivetools/config.py
@@ -265,9 +265,10 @@ def _load_periodic_event(events_dict, config):
     return PeriodicEvent.load(events_dict, config)
 
 
-def _load_manager(man_dict, config):
+def _load_manager(man_file, config):
+    man_dict, _ = DescriptorType.load_any(man_file)
     evaluate_rules(os.path.join("default", "manager.yaml"), man_dict)
-    return Manager.load(man_dict, config)
+    return Manager.load(man_file, man_dict, config)
 
 
 def evaluate_rules(rules_file, dict):
--- reactivetools/manager.py
+++ reactivetools/manager.py
@@ -4,36 +4,38 @@ import json
 
 from . import tools
 from . import glob
+from .descriptor import DescriptorType
 
 class Manager:
     lock = asyncio.Lock()
 
-    def __init__(self, host, port, key):
+    def __init__(self, file, host, port, key):
+        self.config = file
         self.host = host
         self.port = port
         self.key = key
         self.sp_pubkey = None
 
-        self.config = tools.create_tmp(suffix=".json")
-        with open(self.config, "w") as f:
-            json.dump(self.dump(), f)
-
 
     @staticmethod
-    def load(man_dict, config):
+    def load(man_file, man_dict, config):
         host = man_dict['host']
         port = man_dict['port']
         key = man_dict['key']
 
-        return Manager(host, port, key)
+        return Manager(man_file, host, port, key)
 
 
     def dump(self):
-        return {
+        man = {
             "host": self.host,
             "port": self.port,
             "key": self.key
         }
+
+        DescriptorType.YAML.dump(self.config, man)
+        return self.config
+
 
     async def get_sp_pubkey(self):
         async with self.lock:
