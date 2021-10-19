63d69d0013fd370676e3c9f7a2bb04bfcc89f28b
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-10-19 18:17:11 +0200

    sancus: fix symtab file dir

--- reactivetools/nodes/sancus.py
+++ reactivetools/nodes/sancus.py
@@ -88,7 +88,7 @@ class SancusNode(Node):
                             .format(module.name, self.name))
 
         symtab = res.message.payload[2:]
-        symtab_file = tools.create_tmp(suffix='.ld', dir=module.name)
+        symtab_file = tools.create_tmp(suffix='.ld', dir=module.out_dir)
 
         # aiofile for write operations is bugged (version 3.3.3)
         # I get a "bad file descriptor" error after writes.
